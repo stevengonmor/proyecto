2056c45148640a9b2a5f4ebb8ff65f4cbae62e60
Steven <stevengonmor@gmail.com>
2021-08-14 16:46:35 -0600

    Se define un array de rooms y el constructor con un HttpClient para el servicio

--- src/app/rooms/rooms.service.ts
+++ src/app/rooms/rooms.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Room } from './rooms.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
+  private rooms: Room[] = [];
+  constructor(private httpClient: HttpClient) {
+  this.rooms = this.getAll();
+  }
 
-  constructor() { }
 }
