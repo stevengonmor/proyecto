763b55561721fce2a13c0ade2455885cbe34fd48
Steven <stevengonmor@gmail.com>
2021-08-14 17:20:41 -0600

    Se agrega la funcion de getAll al service de rooms

--- src/app/rooms/rooms.service.ts
+++ src/app/rooms/rooms.service.ts
@@ -10,5 +10,28 @@ export class RoomsService {
   constructor(private httpClient: HttpClient) {
   this.rooms = this.getAll();
   }
+  getAll(){
+    this.httpClient.get<{ [key: string]: Room }>("https://hotelapp-91f45-default-rtdb.firebaseio.com/rooms.json")
+    .subscribe(
+        restData => {
+          const productos = [];
+          for (const key in restData){
+            if(restData.hasOwnProperty(key)){
+              productos.push(new Room(
+                key,
+                restData[key].title,
+                restData[key].ocupation,
+                restData[key].status,
+                restData[key].description,
+                restData[key].price,
+                restData[key].img,
+                ));
+            }
+          }
+          this.rooms = productos;
+        }
+    );
+    return [...this.rooms];
+  }
 
 }
