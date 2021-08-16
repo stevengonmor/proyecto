bfc09304f132aebf2a0977aacc6bf2277c6736dc
Steven <stevengonmor@gmail.com>
2021-08-15 18:38:43 -0600

    Se agrega una base visual para Room y se define el ts de rRoom

--- src/app/rooms/room/room.page.ts
+++ src/app/rooms/room/room.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Room } from '../rooms.model';
+import { RoomsService } from '../rooms.service';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.page.scss'],
 })
 export class RoomPage implements OnInit {
-
-  constructor() { }
+  room: Room;
+  constructor(private activatedRoute: ActivatedRoute,
+    private roomsService: RoomsService,
+    private router: Router,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(
+      paramMap => {
+        if(!paramMap.has('roomsId')){
+          return;
+        }
+        const roomId = paramMap.get('roomsId');
+        this.room = this.roomsService.getRoom(roomId);
+      }
+    );
   }
-
 }
