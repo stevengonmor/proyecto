994655256587788ec4e4d036320c16cd377dd56a
Steven <stevengonmor@gmail.com>
2021-08-24 16:58:15 -0600

    Se define el html y el page.ts del login

--- src/app/user/login/login.page.ts
+++ src/app/user/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, NavController } from '@ionic/angular';
+import { RoomsService } from 'src/app/rooms/rooms.service';
+import { FirestorageService } from 'src/app/service/firestorage.service';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  user: User;
+  constructor(
+    private userService: UserService,
+    private roomsService: RoomsService,
+    private router: Router,
+    public firestorageService: FirestorageService,
+    private alert: AlertController,
+    private nav: NavController
+  ) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      })
+    });
+  }
+
+  logIn(){
+    if(!this.form.valid) {return;}
+    this.user = this.userService.logIn(this.form.value.email, this.form.value.password);
+    if(!this.user){
+      this.alert.create({
+        header: 'Cuidado',
+        message: 'Credenciales incorrectas',
+        buttons: [{
+          text: 'Ok',
+          role: 'cancel'
+        }]
+      }).then(
+        alert => {
+          alert.present();
+        }
+      );
+    } else{
+      this.roomsService.getAll();
+    setTimeout(() => {
+      this.router.navigate(['/rooms/confirmation']); //check
+    }, 500);
+    }
   }
 
 }
