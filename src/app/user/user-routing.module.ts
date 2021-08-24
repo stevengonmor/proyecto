2d4c6710096b9bff2e59a3989353e95083429fab
Steven <stevengonmor@gmail.com>
2021-08-24 16:21:47 -0600

    Se crea un page para el login y se actualizan algunos nombres en el html de add de users

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'add',
     loadChildren: () => import('./add/add.module').then( m => m.AddPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
